3b0f21a03c33f43938b7522992e6f971a19efbfe
nathannolacio <nathannolacio04@gmail.com>
2025-07-23 09:11:39 -0300

    :wrench: fix: ajustando import da logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,9 @@
+import logo from "../assets/logo.svg";
+
 export default function Logo() {
     return (
         <div className="flex items-center justify-center gap-2">
-            <img className="size-12" src="/src/assets/meusaldo-icon.svg" alt="Logo do Meu Saldo" />
+            <img className="size-12" src={logo} alt="Logo" />
             <p className="text-2xl font-bold text-green-500">Meu Saldo</p>
         </div>
     )
